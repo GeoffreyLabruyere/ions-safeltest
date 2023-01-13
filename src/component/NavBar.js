717e48d4b4922afb02c07d9c67b8fb1f85aa6371
Geoffrey <geoffrey.labruyere@epitech.eu>
2023-01-13 15:01:36 +0100

    [ADD] responsive logo + bar

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import SAFELlogo from './SAFEL.png';
+import './NavBar.css';
 
 function NavBar() {
   const {click, setClick} = useState(false);
@@ -12,29 +13,14 @@ function NavBar() {
   return (
     <>
       <nav className='navbar'>
-        <div className='navbar-container'>
-          <img src={SAFELlogo} width="300px" height="100px" />
+        <div className='navbar-top'>
+          <div className='navbar-container'>
+            <img src={SAFELlogo} className='resp-img'/>
+          </div>
+          <div className='buttons'>
+            <Link to="/about" className='resp-button'>About</Link>
+          </div>
         </div>
-        <div className='menu-icon' onClick={handleClick}>
-            <i className={click ? 'fas fa-times': 'fas fa-bars'} />
-        </div>
-        <ul className='menu-icon' onClick={handleClick}>
-            <li className='nav-item'>
-                <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
-                  Services
-                </Link>
-            </li>
-            <li className='nav-item'>
-                <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
-                  Products
-                </Link>
-            </li>
-            <li className='nav-item'>
-                <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
-                  Sign up
-                </Link>
-            </li>
-        </ul>
       </nav>
     </>
   );
