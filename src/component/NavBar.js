bbec4ab49438c66a457ab975ba072ecdbd9b80b1
Geoffrey <geoffrey.labruyere@epitech.eu>
2023-01-16 01:10:33 +0100

    [ADD] test for nav

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,14 +1,12 @@
-import React, {useState} from 'react';
+import React, { useState} from 'react';
 import {Link} from 'react-router-dom';
 import SAFELlogo from './SAFEL.png';
 import './NavBar.css';
+import Banner from './Banner'
 
 function NavBar() {
-  const {click, setClick} = useState(false);
 
-  const handleClick = () => setClick(!click);
-
-  const closeMobileMenu = () => setClick(false);
+  const [cls, setCls] = useState("green");
 
   return (
     <>
@@ -16,10 +14,25 @@ function NavBar() {
         <div className='navbar-top'>
           <div className='navbar-container'>
             <img src={SAFELlogo} className='resp-img'/>
+            <span>
+              <Link to="/about" className='resp-button'>About</Link>
+              <Link to="/about" className='resp-button'>About2</Link>
+              <Link to="/about" className='resp-button'>About3</Link>
+            </span>
           </div>
-          <div className='buttons'>
-            <Link to="/about" className='resp-button'>About</Link>
-          </div>
+            <style>{`
+            .red {color: red}
+            .green {color: green}
+          `}</style>
+          <button
+            className={cls}
+            onClick={() => setCls((cls) => (cls === "red" ? "green" : "red"))}
+          >
+            ButtonZDZD
+          </button>
+          <Link to="/another-page">
+            <span className="navbar-button">Button</span>
+          </Link>
         </div>
       </nav>
     </>
