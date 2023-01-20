8887ae5ee5d67686412fc4b4415e8c3b50c9b90d
Geoffrey <geoffrey.labruyere@epitech.eu>
2023-01-20 14:42:59 +0100

    UPDATE visual

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 import SAFELlogo from './SAFELBLUE.png';
 import './NavBar.css';
 import SearchBar from './SearchBar';
+import LoginButton from './Login';
 
 
 function NavBar() {
@@ -13,12 +14,13 @@ function NavBar() {
             <div className='navbar-container'>
               <img src={SAFELlogo} className='resp-img'/>
               <SearchBar/>
-              <div className="navigation">
+              <div>
                 <Link to="/" className="navigation-button">Accueil</Link>
                 <Link to="/products" className="navigation-button">Nos produits</Link>
                 <Link to="/about-us" className="navigation-button">À propos de nous</Link>
                 <Link to="/contact-us" className="navigation-button">Nous contacter</Link>
               </div>
+             <Link to="/sign-in" className="sign">Se connecter</Link>
           </div>
         </div>
       </nav>
